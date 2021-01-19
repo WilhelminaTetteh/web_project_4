9600bc835e40275cf6ceaa7bad1903efdf098fb9
Wilhelmina Tetteh <yemoleytetteh333@gmail.com>
2021-01-19 03:23:01 -0800

    Project 4

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 let editButton = document.querySelector(".profile__edit");
+let modal = document.querySelector(".modal");
 let closeButton = document.querySelector(".modal__button");
 let form = document.querySelector(".form");
 let titleInput = document.querySelector(".form__input_type_title");
@@ -6,20 +7,27 @@ let descriptionInput = document.querySelector(".form__input_type_description");
 let profileTitle = document.querySelector(".profile__title");
 let profileDescription = document.querySelector(".profile__description");
 
+
+// open modal on click event
+editButton.addEventListener('click',function() {
+    
+    modal.classList.add("modal_open");
+})
+
+// close modal on click event
+
+closeButton.addEventListener('click',function() {
+    
+    modal.classList.remove("modal_open");
+})
+
+// replace info on submit event and close
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     profileTitle.textContent = titleInput.value;
     profileDescription.textContent = descriptionInput.value;
 
-    toggleModal();
+    modal.classList.remove("modal_open");
 
-    console.log("submit!!", titleInput.value, descriptionInput.value);
 });
-
-function toggleModal() {
-    let modal = document.querySelector(".modal");
-    modal.classList.toggle("modal_open");
-}
-
-editButton.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
