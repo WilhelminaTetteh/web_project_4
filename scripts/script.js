7a4aa0373488153758b729e016fdb3df84d1376c
Wilhelmina Tetteh <yemoleytetteh333@gmail.com>
2021-01-21 21:29:22 -0800

    Project 4

--- scripts/script.js
+++ scripts/script.js
@@ -8,10 +8,11 @@ let profileTitle = document.querySelector(".profile__title");
 let profileDescription = document.querySelector(".profile__description");
 
 function openModal() {
-    modal.classList.add("modal_open");
-
     titleInput.value = profileTitle.textContent;
     descriptionInput.value = profileDescription.textContent;
+
+    modal.classList.add("modal_open");
+
 }
 editButton.addEventListener("click", openModal);
 
