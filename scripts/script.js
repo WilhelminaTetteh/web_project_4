ac688a8c8f12702b89bb60ee89b10d4546b8474b
Wilhelmina Tetteh <yemoleytetteh333@gmail.com>
2021-02-01 14:44:18 -0800

    Project 5

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,32 @@
 //open buttons
-let editButton = document.querySelector(".profile__edit");
-let addCardButton = document.querySelector(".profile__add");
+const editButton = document.querySelector(".profile__edit");
+const addCardButton = document.querySelector(".profile__add");
 
 
 //Wrappers-modals
 // let modalWindow = document.querySelector(".modal");
-let editModal = document.querySelector(".modal_type_edit-profile");
-let addCardModal = document.querySelector(".modal_type_add-card");
-let imageModal = document.querySelector(".modal_type_image"); 
+const editModal = document.querySelector(".modal_type_edit-profile");
+const addCardModal = document.querySelector(".modal_type_add-card");
+const imageModal = document.querySelector(".modal_type_image"); 
 
 //Close buttons
-let closeButton = editModal.querySelector(".modal__button"); 
-let cardCloseButton = addCardModal.querySelector(".modal__button"); 
-let closeImageButton = imageModal.querySelector(".modal__button");
+const closeButton = editModal.querySelector(".modal__button"); 
+const cardCloseButton = addCardModal.querySelector(".modal__button"); 
+const closeImageButton = imageModal.querySelector(".modal__button");
 // form inputs etc
-let form = document.querySelector(".form");
-let newCardForm = document.querySelector(".form_type_card");
-let titleInput = document.querySelector(".form__input_type_title");
-let descriptionInput = document.querySelector(".form__input_type_description");
-let profileTitle = document.querySelector(".profile__title");
-let profileDescription = document.querySelector(".profile__description");
+const profileEditForm = document.querySelector(".form");
+const newCardForm = document.querySelector(".form_type_card");
+const titleInput = document.querySelector(".form__input_type_title");
+const descriptionInput = document.querySelector(".form__input_type_description");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
 
-let nameInput = document.querySelector(".form__input_type_card-title");
-let imageInput = document.querySelector(".form__input_type_url");
-let popupImage = imageModal.querySelector(".modal__image"); 
-let imageCaption = imageModal.querySelector(".modal__image-caption"); 
-
+const nameInput = document.querySelector(".form__input_type_card-title");
+const imageInput = document.querySelector(".form__input_type_url");
+const popupImage = imageModal.querySelector(".modal__image"); 
+const imageCaption = imageModal.querySelector(".modal__image-caption"); 
+const cardTemplate = document.querySelector(".grid__template").content.querySelector(".grid__item");
+const cardContainer = document.querySelector(".grid__container");
 
 function toggleModalWindow(modal) {
     modal.classList.toggle("modal_open");
@@ -44,14 +45,14 @@ closeButton.addEventListener("click", () => {
 });
 
 //submit form and close
-function submitForm(event) {
+function submitProfileForm(event) {
     event.preventDefault();
     profileTitle.textContent = titleInput.value;
     profileDescription.textContent = descriptionInput.value;
 
     toggleModalWindow(editModal);
 }
-form.addEventListener("submit", submitForm);
+profileEditForm.addEventListener("submit", submitProfileForm);
 
 //NEW MODAL
 addCardButton.addEventListener("click", () => {
@@ -94,8 +95,6 @@ const initialCards = [
     },
 ];
 
-const cardTemplate = document.querySelector(".grid__template").content.querySelector(".grid__item");
-const cardContainer = document.querySelector(".grid__container");
 
 function createCardElement(name, link){
     const cardElement = cardTemplate.cloneNode(true);
