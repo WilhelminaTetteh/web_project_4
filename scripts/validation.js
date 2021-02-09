682a743bda54d0b92b1000d14835d58190b339ec
Wilhelmina Tetteh <yemoleytetteh333@gmail.com>
2021-02-09 08:18:41 -0800

    Project 6

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,25 +1,40 @@
 const showErrorMessage = function(input, form, {errorClass,inputErrorClass, ...rest}){
-    const error = document.querySelector('#' + '-error');
+    const error = document.querySelector('#' + input.id +'-error');
 
     error.textContent = input.validationMessage;
-    error.classList.add(errorClass)
-    input.classList.add(inputErrorClass)
+    error.classList.add(errorClass);
+    input.classList.add(inputErrorClass);
+}
+const hideErrorMessage = function(input, form, {errorClass,inputErrorClass, ...rest}){
+    const error = document.querySelector('#' + input.id +'-error');
+
+    error.textContent = '';
+    error.classList.remove(errorClass);
+    input.classList.remove(inputErrorClass);
 }
 
 const checkInputValidity = function(input, form, rest){
     if(input.validity.valid){
-        //hideErrorMessage
+        hideErrorMessage(input, form, rest);
     } else {
         showErrorMessage(input, form, rest);
     }
 }
 
+ const toggleButtonState = function(inputs, button, {inactiveButtonClass, ...rest}){
+    const isValid = inputs.every((input) => input.validity.valid)
+    
+    if(isValid){
+        button.classList.remove(inactiveButtonClass)
+    } else{
+        button.classList.add(inactiveButtonClass)
+    }
+}
 
 
 
 const enableValidation =  function({formSelector,inputSelector, submitButtonSelector, ...rest}){
     const forms = [...document.querySelectorAll(formSelector)];
-    console.log("form", forms)
     forms.forEach((form) =>{
         form.addEventListener('submit', ((e) => {
             e.preventDefault()
@@ -30,7 +45,7 @@ const enableValidation =  function({formSelector,inputSelector, submitButtonSele
         inputs.forEach((input) => {
             input.addEventListener('input', () => {
                 checkInputValidity(input, form, rest);
-                // toggleButtonState
+                toggleButtonState(input, button , rest);
             })
         })
     })
@@ -41,7 +56,7 @@ enableValidation({
     formSelector: ".form",
     inputSelector: ".form__input",
     submitButtonSelector: ".form__button",
-    inactiveButtonClass: "form__button_disabled",
-    inputErrorClass: "form__input_type_error",
-    errorClass: "form__error_visible"
+    inactiveButtonClass: ".form__button_disabled",
+    inputErrorClass: ".form__input_type_error",
+    errorClass: ".form__error_visible"
   }); 
