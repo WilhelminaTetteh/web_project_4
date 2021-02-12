caea9c0079877e0bf509532e427a7604a7d5c9d4
Wilhelmina Tetteh <yemoleytetteh333@gmail.com>
2021-02-12 00:07:26 -0800

    Project 6

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,15 @@
 const editButton = document.querySelector(".profile__edit");
 const addCardButton = document.querySelector(".profile__add");
 
-
 //Wrappers-modals
 // let modalWindow = document.querySelector(".modal");
 const editModal = document.querySelector(".modal_type_edit-profile");
 const addCardModal = document.querySelector(".modal_type_add-card");
-const imageModal = document.querySelector(".modal_type_image"); 
+const imageModal = document.querySelector(".modal_type_image");
 
 //Close buttons
-const closeButton = editModal.querySelector(".modal__button"); 
-const cardCloseButton = addCardModal.querySelector(".modal__button"); 
+const closeButton = editModal.querySelector(".modal__button");
+const cardCloseButton = addCardModal.querySelector(".modal__button");
 const closeImageButton = imageModal.querySelector(".modal__button");
 // form inputs etc
 const profileEditForm = document.querySelector(".form");
@@ -23,176 +22,153 @@ const profileDescription = document.querySelector(".profile__description");
 
 const nameInput = document.querySelector(".form__input_type_card-title");
 const imageInput = document.querySelector(".form__input_type_url");
-const popupImage = imageModal.querySelector(".modal__image"); 
-const imageCaption = imageModal.querySelector(".modal__image-caption"); 
+const popupImage = imageModal.querySelector(".modal__image");
+const imageCaption = imageModal.querySelector(".modal__image-caption");
 const cardTemplate = document.querySelector(".grid__template").content.querySelector(".grid__item");
 const cardContainer = document.querySelector(".grid__container");
 
 // open and close Modals
-function openPopup(modalOpen){
-    modalOpen.classList.add("modal_open");
+function openPopup(modalOpen) {
+  modalOpen.classList.add("modal_open");
+  document.addEventListener("keydown", closeByEscape);
 }
 
-function closePopup(modalClose){
-    modalClose.classList.remove("modal_open");
+function closePopup(modalClose) {
+  modalClose.classList.remove("modal_open");
+  document.removeEventListener("keydown", closeByEscape);
 }
 
+
+function closeByEscape(evt) {
+    const activeModal = document.querySelector(".modal_open");
+    if (evt.key === "Escape") {
+      closePopup(activeModal);
+    }
+  }
 
 // Input data then Open
 function openModal() {
-    titleInput.value = profileTitle.textContent;
-    descriptionInput.value = profileDescription.textContent;
-    openPopup(editModal);
+  titleInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+  openPopup(editModal);
 }
 editButton.addEventListener("click", openModal);
 
 //close modal
 closeButton.addEventListener("click", () => {
-    closePopup(editModal);
+  closePopup(editModal);
 });
 
 //submit form and close
 function submitProfileForm(event) {
-    event.preventDefault();
-    profileTitle.textContent = titleInput.value;
-    profileDescription.textContent = descriptionInput.value;
+  event.preventDefault();
+  profileTitle.textContent = titleInput.value;
+  profileDescription.textContent = descriptionInput.value;
 
-    closePopup(editModal);
+  closePopup(editModal);
 }
 profileEditForm.addEventListener("submit", submitProfileForm);
 
 //NEW MODAL
 addCardButton.addEventListener("click", () => {
-    openPopup(addCardModal);
+  openPopup(addCardModal);
 });
 cardCloseButton.addEventListener("click", () => {
-    closePopup(addCardModal);
+  closePopup(addCardModal);
 });
 // close image
 closeImageButton.addEventListener("click", () => {
-    closePopup(imageModal);
+  closePopup(imageModal);
 });
 
 // SPRINT 5
 
 const initialCards = [
-    {
-        name: "Lago di Braies",
-        link: "https://code.s3.yandex.net/web-code/lago.jpg",
-    },
-    {
-        name: "Vanoise National Park",
-        link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-    },
-    {
-        name: "Latemar",
-        link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-    },
-    {
-        name: "Bald Mountains",
-        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-    },
-    {
-        name: "Lake Louise",
-        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-    },
-    {
-        name: "Yosemite Valley",
-        link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-    },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
 ];
 
+function createCardElement(name, link) {
+  const cardElement = cardTemplate.cloneNode(true);
 
-function createCardElement(name, link){
-    const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".grid__image");
+  const cardText = cardElement.querySelector(".grid__text");
+  const cardLikeIcon = cardElement.querySelector(".grid__icon");
+  const cardDeleteIcon = cardElement.querySelector(".grid__delete-icon");
 
-    const cardImage = cardElement.querySelector(".grid__image");
-    const cardText = cardElement.querySelector(".grid__text");
-    const cardLikeIcon = cardElement.querySelector(".grid__icon");
-    const cardDeleteIcon = cardElement.querySelector(".grid__delete-icon");
-    
-    cardText.textContent = name;
-    cardImage.style.backgroundImage = `url(${link})`;
-   
-    cardLikeIcon.addEventListener("click", () => {
-        cardLikeIcon.classList.toggle("grid__icon_active");
-    });
-    
-    cardDeleteIcon.addEventListener("click", () => {
-        cardElement.remove();
-    });
-    
-    cardImage.addEventListener("click", () => {
-        popupImage.src = link;
-        popupImage.alt = name;
-        imageCaption.textContent = name;
-        openPopup(imageModal);
-    });
-    return cardElement;
+  cardText.textContent = name;
+  cardImage.style.backgroundImage = `url(${link})`;
+
+  cardLikeIcon.addEventListener("click", () => {
+    cardLikeIcon.classList.toggle("grid__icon_active");
+  });
+
+  cardDeleteIcon.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  cardImage.addEventListener("click", () => {
+    popupImage.src = link;
+    popupImage.alt = name;
+    imageCaption.textContent = name;
+    openPopup(imageModal);
+  });
+  return cardElement;
 }
 
 initialCards.forEach((data) => {
-    const cardElement = createCardElement(data.name ,data.link)
-    cardContainer.prepend(cardElement);
+  const cardElement = createCardElement(data.name, data.link);
+  cardContainer.prepend(cardElement);
 });
 
-
-
 function addCard(event) {
-    event.preventDefault();
-    const cardElement = createCardElement( nameInput.value ,`${imageInput.value}`);
-  
-    cardContainer.prepend(cardElement);
+  event.preventDefault();
+  const cardElement = createCardElement(nameInput.value, `${imageInput.value}`);
 
-    nameInput.value = "";
-    imageInput.value = "";
+  cardContainer.prepend(cardElement);
 
-    closePopup(addCardModal);     
+  closePopup(addCardModal);
 }
 newCardForm.addEventListener("submit", addCard);
 
-
-
-
 //close popups when  click on overlay
 
-editModal.addEventListener('click', (evt) => {
-    if(evt.target === editModal){
-        closePopup(editModal);
-    }
-    
+editModal.addEventListener("click", (evt) => {
+  if (evt.target === editModal) {
+    closePopup(editModal);
+  }
 });
-addCardModal.addEventListener('click', (evt) => {
-    if(evt.target === addCardModal){
-        closePopup(addCardModal);
-    }
-    
+addCardModal.addEventListener("click", (evt) => {
+  if (evt.target === addCardModal) {
+    closePopup(addCardModal);
+  }
 });
-imageModal.addEventListener('click', (evt) => {
-    if(evt.target === imageModal){
-        closePopup(imageModal);
-    }
-    
+imageModal.addEventListener("click", (evt) => {
+  if (evt.target === imageModal) {
+    closePopup(imageModal);
+  }
 });
 
-// document.addEventListener('keyup', (evt) => {
-//     if(evt.key === 'Escape') {
-//         closePopup(editModal);
-//         closePopup(addCardModal);
-//         closePopup(imageModal);
-//     }
-// });
 
-
-// Escape Functionality
-function closeByEscape(evt){
-    if(evt.key === 'Escape') {
-
-        closePopup(editModal);
-        closePopup(addCardModal);
-        closePopup(imageModal);
-    }
-}
-
-document.addEventListener('keydown',closeByEscape);
-document.removeEventListener('keydown',closeByEscape);
